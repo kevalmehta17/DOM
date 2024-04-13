58cbd3dabeedc3095287e254522738ea202dbfdf
Keval <kevalmehta885@gmail.com>
2024-04-13 12:12:40 +0530

    Added index.js File

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("my name is keval");
+const express = require("express");
+const app = express();
+const port = 2000;
+
+app.get("/sum", (req, res) => {
+  const a = req.query.a;
+  const b = req.query.b;
+  const total = parseInt(a) + parseInt(b);
+  res.send(total.toString());
+});
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
